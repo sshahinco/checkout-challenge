8a6c81541967ac495063ab9dd2c6b5e1613862a9
Sherief Shahin <sheriefshahin@Sheriefs-MacBook-Pro.local>
2022-09-07 10:07:01 +0200

    Initialized backend project

---
+++ checkout-server/src/test/java/com/example/checkoutserver/CheckoutServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.checkoutserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckoutServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
